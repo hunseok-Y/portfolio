0da9999473159e0b435c7f00c79185084270460a
yanghoonseok <gnstjr5911@gmail.com>
2025-10-28 18:04:04 +0900

    탭버튼 수정

--- src/components/tab.tsx
+++ src/components/tab.tsx
@@ -8,10 +8,12 @@ export default function Tab() {
 		<>
 			<div className=" w-[150px] ">
 				<div className="flex flex-col items-start gap-y-2.5 sticky top-24">
-					<button onClick={() => navigate("/")} className={`text-[18px]  text-[#bbbbbb]  hover:underline ${location == "/" ? "font-[900] text-white" : "font-[400]"}`}>
+					<button onClick={() => navigate("/")} className={`text-[18px] text-[#bbbbbb] cursor-pointer hover:underline ${location == "/" ? "font-[900] text-white" : "font-[400]"}`}>
 						HOME
 					</button>
-					<button onClick={() => navigate("/project")} className={`text-[18px]  text-[#bbbbbb] hover:underline ${location == "/project" ? "font-[900] text-white" : "font-[400]"}`}>
+					<button
+						onClick={() => navigate("/project")}
+						className={`text-[18px] cursor-pointer text-[#bbbbbb] hover:underline ${location == "/project" ? "font-[900] text-white" : "font-[400]"}`}>
 						PROJECT
 					</button>
 				</div>
